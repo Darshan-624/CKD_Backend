c48b9ec11099b858438cb2503c727fa3cec3de85
Darshan-624 <darshandacchu2402@gmail.com>
2026-01-11 21:24:32 +0530

    bhai dheklega

--- app/core/config.py
+++ app/core/config.py
@@ -5,10 +5,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings(BaseSettings):
-    SUPABASE_URL: str = os.getenv("SUPABASE_URL")
-    SUPABASE_KEY: str = os.getenv("SUPABASE_KEY")
-    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    SUPABASE_URL: str
+    SUPABASE_KEY: str
+    SECRET_KEY: str
     EXPIRATION_TIME_SECONDS: int = 3600 # Default to 1 hour if not set
+
+    class Config:
+        env_file = ".env"
+        case_sensitive = True
 
 
 settings = Settings()
